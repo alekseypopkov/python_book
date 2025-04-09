52f7e89b79c4b3eca4131de5e2f629dd1c8ddc2c
Alexey Popkov <alvlpv969@gmail.com>
2025-04-09 15:01:30 +1000

    Строки 3.1

--- Часть1-Основы/Строки/symbol.py
+++ Часть1-Основы/Строки/symbol.py
@@ -8,8 +8,8 @@ print()
 # Разрывы строк добавляются с помощью комбинации символов \n:
 print("Languages:\nPython\nC\nJavaScript")
 print()
+
 # В следующем примере показано, как использо-вать 
 # одну строку кода для создания четырех строк вывода:
-
 print("Languages:\n\tPython\n\tC\n\tJavaScript")
 print()
