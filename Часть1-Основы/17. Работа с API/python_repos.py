b3e1fc3855a9af87bbe88e28350cae8630173d6f
Tara Routray <dev@tararoutray.com>
2025-04-20 15:33:50 +1000

    numpy

--- Часть1-Основы/17. Работа с API/python_repos.py
+++ Часть1-Основы/17. Работа с API/python_repos.py
@@ -4,13 +4,9 @@ from pprint import pprint
 
 # Создание вызова API и сохранение ответа.
 url = "https://api.github.com/search/repositories"
-
 url += "?q=language:python+sort:stars+stars:>10000"
-
 headers = {"Accept": "application/vnd.github.v3+json"}
-
 r = requests.get(url, headers=headers)
-
 print(f"Status code: {r.status_code}")
 
 # Преобразование объекта ответа в словарь.
